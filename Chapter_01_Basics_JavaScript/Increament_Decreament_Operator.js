bf6a5b9b3d4188a7d26bc61200ea0801e30c4d41
Anand Sinha <anand.sinha@example.com>
2026-07-20 19:08:06 +0530

    Ternary and Inc_Decrement

---
+++ Chapter_01_Basics_JavaScript/Increament_Decreament_Operator.js
@@ -0,0 +1,14 @@
+//Pre increment
+
+// let a=10;
+// let b = ++a;
+// console.log(b);
+
+// //Post increment
+// let c = 10;
+// let d=c++;
+// console.log()
+
+let a = 10;
+console.log(++a + a + a++);
+console.log(a);
