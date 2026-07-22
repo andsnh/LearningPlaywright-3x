72de0fbe938282d41627611c67fd3f284ba6025c
Anand Sinha <sunnyt045@gmail.com>
2026-07-22 20:15:16 +0530

    Hacker Rank Challenge commit

--- Chapter_01_Basics_JavaScript/HelloWorld.js
+++ Chapter_01_Basics_JavaScript/HelloWorld.js
@@ -1,1 +1,18 @@
-console.log("Hello Anand Sinha");
+let x = 15;
+
+for (let i = 1; i <= x; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+        console.log("FizzBuzz");
+    }
+
+    else if (i % 3 == 0) {
+        console.log("Fizz");
+    }
+    else if (i % 5 == 0) {
+        console.log("Buzz");
+    }
+
+    else {
+        console.log(i);
+    }
+}
